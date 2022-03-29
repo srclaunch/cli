af51e2e8ba2d0e874cd4ce7fb4e9ab63ac6bb747
Steven Bennett <steven@budgetbloom.com>
2022-03-28 19:46:33 -0700

    Quick release - patch

--- src/commands/models/build/outputs/types.ts
+++ src/commands/models/build/outputs/types.ts
@@ -131,23 +131,11 @@ export async function buildModelTypes(projectPath: string) {
   try {
     const MODELS_BUILD_PATH = path.join(
       path.resolve(),
-      '.applab/dependencies/models/dist/index',
+      '.applab/dependencies/models/dist/index.js',
     );
-
-    console.log('projectPath', projectPath);
-
-    console.log('MODELS_BUILD_PATH', MODELS_BUILD_PATH);
     const BUILD_PATH = path.join(path.resolve(), projectPath, 'src');
-
-    console.log('BUILD_PATH', BUILD_PATH);
-
     const DIST_PATH = path.join(path.resolve(), projectPath, 'dist');
-
-    console.log('DIST_PATH', DIST_PATH);
-
     const TYPES_DIR_PATH = path.join(path.resolve(), 'types');
-
-    console.log('TYPES_DIR_PATH', TYPES_DIR_PATH);
 
     await fs.emptyDir(BUILD_PATH);
     await fs.emptyDir(DIST_PATH);
@@ -164,8 +152,6 @@ export async function buildModelTypes(projectPath: string) {
     }
 
     const Models = await import(MODELS_BUILD_PATH);
-
-    console.log('Models', Models);
 
     let exportStr = '';
 
