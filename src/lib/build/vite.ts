1bfef3d6f51064e050909f9a3a611c475f34b07e
Steven Bennett <steven@budgetbloom.com>
2022-04-15 14:16:33 -0700

    Quick release - patch

--- src/lib/build/vite.ts
+++ src/lib/build/vite.ts
@@ -86,6 +86,7 @@ export async function build({
             ? (bundle?.optimize as string[]) ?? []
             : [],
       },
+      plugins: webApp?.react ? [react()] : [],
       root: path.join(path.resolve(), rootDir),
     });
 
