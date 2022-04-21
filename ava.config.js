1d3cf40eee95eda0963187c423c0d3e4bd39437d
Steven Bennett <steven@budgetbloom.com>
2022-04-21 07:22:17 -0700

    Quick release - patch

--- ava.config.js
+++ ava.config.js
@@ -1,14 +1,12 @@
 export default {
-  "failFast": true,
-  "verbose": true,
-	"extensions": {
-    "ts": "module"
+  failFast: true,
+  verbose: true,
+  extensions: {
+    ts: 'module',
   },
-  "nodeArguments": [
-    "--loader=ts-node/esm",
-    "--experimental-specifier-resolution=node"
+  nodeArguments: [
+    '--loader=ts-node/esm',
+    '--experimental-specifier-resolution=node',
   ],
-  "require": [
-    "ts-node/register/transpile-only"
-  ]
+  require: ['ts-node/register/transpile-only'],
 };
