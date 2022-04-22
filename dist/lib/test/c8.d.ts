bf910daa83e8bec5764a0106353d824ecbf8efca
Steven Bennett <steven@budgetbloom.com>
2022-04-21 20:28:09 -0700

    Quick release - patch

--- dist/lib/test/c8.d.ts
+++ dist/lib/test/c8.d.ts
@@ -1,3 +1,4 @@
 import { TestOptions } from '@srclaunch/types';
-export declare function run(config: TestOptions): Promise<void>;
+import { Report } from 'c8';
+export declare function run(config: TestOptions, srcPath?: string): Promise<Report>;
 //# sourceMappingURL=c8.d.ts.map
--- src/lib/test/c8.ts
+++ src/lib/test/c8.ts
@@ -1,16 +1,22 @@
 import { TestOptions } from '@srclaunch/types';
 import { Report } from 'c8';
+import path from 'path';
 
-export async function run(config: TestOptions) {
+export async function run(
+  config: TestOptions,
+  srcPath?: string,
+): Promise<Report> {
   try {
     const report = new Report({
       all: true,
-      include: 'src',
+      src: [path.join(process.cwd(), srcPath ?? 'src')],
       reporter: ['json'],
     });
 
     await report.run();
+
+    return report;
   } catch (err) {
-    console.error(err);
+    throw err;
   }
 }
