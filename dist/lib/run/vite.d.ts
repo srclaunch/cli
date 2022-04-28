015450b28bf6f2ce4a4e6918b26f0829e1d66963
Steven Bennett <steven@budgetbloom.com>
2022-04-27 19:14:36 -0700

    Remove gitignored

--- dist/lib/run/vite.d.ts
+++
@@ -1,6 +0,0 @@
-import { Environments, WebApplicationRunOptions } from '@srclaunch/types';
-export declare function run({ options, environment, }: {
-    options?: WebApplicationRunOptions['development' | 'preview' | 'qa' | 'production'];
-    environment: Environments;
-}): Promise<void>;
-//# sourceMappingURL=vite.d.ts.map
--- src/lib/build/vite.ts
+++ src/lib/build/vite.ts
@@ -63,7 +63,6 @@ export async function build({
             }
           : false,
         manifest,
-
         minify,
         rollupOptions: {
           external: (typeof bundle === 'object'
