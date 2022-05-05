89aa441490c1e0c391b4efe21e2f118863a22da3
Steven Bennett <steven@budgetbloom.com>
2022-05-05 16:20:07 -0700

    Quick release - patch

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,4 +1,4 @@
-import { readFile } from './file-system';
+import { deleteFile, readFile, writeFile } from './file-system';
 import path from 'node:path';
 import ts from 'typescript';
 
@@ -7,23 +7,29 @@ export async function getSrcLaunchConfig() {
     // const configFormats = ['js', 'json', 'ts'];
     try {
       const configPath = path.join(path.resolve(), './.srclaunch/config.ts');
-      const config = await readFile(configPath);
+      const tempPath = path.join(path.resolve(), './.srclaunch/.tmp/config.js');
+      const configContents = await readFile(configPath);
 
-      let result = await ts.transpileModule(config.toString(), {
+      let result = await ts.transpileModule(configContents.toString(), {
         compilerOptions: { module: ts.ModuleKind.ESNext },
       });
 
-      console.log(result.outputText);
+      await writeFile(tempPath, result.outputText);
+
+      const config = await import(tempPath);
+
+      console.log(config);
       // let result = require(configPath);
       // if (result && result.__esModule && result.default) {
       //   result = result.default;
       // }
       // return result;
+      await deleteFile(tempPath);
     } catch (tsImportError: any) {
       console.log('tsImportError', tsImportError);
       try {
         const configPath = path.join(path.resolve(), './.srclaunch/config.js');
-        let result = require(configPath);
+        let result = await import(configPath);
         if (result && result.__esModule && result.default) {
           result = result.default;
         }
