4e81c4fff6aeb4518f9b84ac8348c6f9e32dcef8
Steven Bennett <steven@budgetbloom.com>
2022-05-13 20:57:04 -0700

    Quick release

--- src/lib/project/dependencies.ts
+++ src/lib/project/dependencies.ts
@@ -465,6 +465,7 @@ export async function getDependencies({
   dev?: boolean;
   packages?: Package[];
 }): Promise<Dependencies> {
+  console.log('DEV: ', dev);
   console.log('packages', packages);
   if (!packages) {
     return {};
